899577ce1ef788e4fbe755d0850f1e77d1fc1759
langdon <getpacman.gq@gmail.com>
2026-05-21 14:46:20 -0400

    Add files for Intellij

--- src/org/simplenativehooks/NativeHookInitializer.java
+++ src/org/simplenativehooks/NativeHookInitializer.java
@@ -12,7 +12,7 @@ import java.util.logging.Logger;
 public class NativeHookInitializer {
 
     public static final String VERSION = "1.0.0";
-    public static final boolean USE_X11_ON_LINUX = false;
+    public static final boolean USE_X11_ON_LINUX = true;
     private static final Logger LOGGER = Logger.getLogger(NativeHookInitializer.class.getName());
     public void start() {
         if (OSIdentifier.IS_WINDOWS) {
