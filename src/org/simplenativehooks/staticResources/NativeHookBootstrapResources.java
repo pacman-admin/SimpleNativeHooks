498248b8d506c1aa1d1ec3b31229f252ef781826
langdon <getpacman.gq@gmail.com>
2026-05-23 11:07:20 -0400

    Code cleanup

--- src/org/simplenativehooks/staticResources/NativeHookBootstrapResources.java
+++ src/org/simplenativehooks/staticResources/NativeHookBootstrapResources.java
@@ -7,11 +7,15 @@ import org.simplenativehooks.utilities.OSIdentifier;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.logging.Logger;
 
-public class NativeHookBootstrapResources extends AbstractBootstrapResource {
+public class NativeHookBootstrapResources {
+    private static final Logger LOGGER = Logger.getLogger(NativeHookBootstrapResources.class.getName());
+
     public static void extract() throws IOException, URISyntaxException {
         new NativeHookBootstrapResources().extractResources();
     }
+
     public static File getNativeHookDirectory() {
         return new File(FileUtility.joinPath("resources", "nativehooks", getOSDir()));
     }
@@ -46,8 +50,17 @@ public class NativeHookBootstrapResources extends AbstractBootstrapResource {
         throw new IllegalStateException("OS is unsupported.");
     }
 
-    @Override
-    protected boolean postProcessing(String name) {
+    private void extractResources() throws IOException, URISyntaxException {
+        if (!FileUtility.createDirectory(getExtractingDest().getAbsolutePath())) {
+            LOGGER.warning("Failed to extract " + getName() + " resources");
+            return;
+        }
+
+        final String path = getRelativeSourcePath();
+        FileUtility.extractFromCurrentJar(path, getExtractingDest(), this::correctExtension, this::postProcessing);
+    }
+
+    private boolean postProcessing(String name) {
         if (OSIdentifier.IS_LINUX) {
             if (NativeHookInitializer.USE_X11_ON_LINUX) {
                 if (name.endsWith("RepeatHookX11Key.out") || name.endsWith("RepeatHookX11Mouse.out")) {
@@ -63,8 +76,8 @@ public class NativeHookBootstrapResources extends AbstractBootstrapResource {
         return true;
     }
 
-    @Override
-    protected boolean correctExtension(String name) {
+
+    private boolean correctExtension(String name) {
         if (OSIdentifier.IS_WINDOWS) {
             return name.endsWith("RepeatHook.exe");
         }
@@ -81,18 +94,16 @@ public class NativeHookBootstrapResources extends AbstractBootstrapResource {
         throw new IllegalStateException("OS is unsupported.");
     }
 
-    @Override
-    protected String getRelativeSourcePath() {
+
+    private String getRelativeSourcePath() {
         return "org/simplenativehooks/" + getOSDir() + "/nativecontent";
     }
 
-    @Override
-    protected File getExtractingDest() {
+    private File getExtractingDest() {
         return new File(FileUtility.joinPath("resources", "nativehooks", getOSDir()));
     }
 
-    @Override
-    protected String getName() {
+    private String getName() {
         return "NativeHook";
     }
 }
